7d91e5f99831c5b641fa1f2997885c922f6bd660
Robert James Hernandez <rob@sarcasticadmin.com>
2021-09-13 22:09:00 -0700

    Initializing and adding aprx pkg

---
+++ pkgs/aprx/default.nix
@@ -0,0 +1,31 @@
+{ lib, stdenv, fetchgit, perl}:
+#with (import <nixpkgs> {});
+
+stdenv.mkDerivation rec {
+  pname = "aprx-unstable";
+  version = "2021-06-01";
+
+  src = fetchgit {
+    url = "https://github.com/PhirePhly/aprx";
+    rev = "13c6e7950210c501f71a0d12bb5c780f37e21b9b";
+    sha256 = "1g6di35r3l4vwarrfaymhf3qhyzd399dn3hnya6ydccph4np61x0";
+  };
+
+  nativeBuildInputs = [  ];
+
+  buildInputs = [ perl ];
+  
+  installPhase = ''
+    mkdir -p $out/bin
+    cp aprx $out/bin
+    install -D -m 644 aprx.8 $out/share/man/man8/aprx.8
+  '';
+
+  meta = with lib; {
+    description = "A multitalented APRS i-gate / digipeater";
+    homepage = "http://thelifeofkenneth.com/aprx";
+    license = licenses.bsd3;
+    maintainers =  with maintainers; [ sarcasticadmin ];
+    platforms = platforms.linux;
+  };
+}
