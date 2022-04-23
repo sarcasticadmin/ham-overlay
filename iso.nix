140803dfaae450a8d92e8c6b2961a1281cc2f8a1
Robert James Hernandez <rob@sarcasticadmin.com>
2022-04-22 23:58:42 -0700

    Making iso support /etc/ax25

--- iso.nix
+++ iso.nix
@@ -9,14 +9,31 @@ with nixpkgs;
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
-  # Installs all necessary packages for the iso
-  environment.systemPackages = [
-    aprx
-    tncattach
-    libax25
-    pat
-    flashtnc
-  ];
+  environment = {
+    # Installs all necessary packages for the iso
+    systemPackages = [
+      aprx
+      tncattach
+      libax25
+      pat
+      flashtnc
+    ];
+
+    # libax25, etc. are set to assume the common config path
+    # TODO: Definitely need to come up with a beter way to deal with this
+    etc."ax25/axports" = {
+      text = ''
+      # me callsign speed paclen window description
+      #
+      wl2k km6lbu-6 57600 255 7 Winlink
+      '';
+
+      # The UNIX file mode bits
+      mode = "0644";
+    };
+
+  };
+
   # use the latest Linux kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
