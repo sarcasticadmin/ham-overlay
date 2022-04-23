e52c79612084a05bafbfde40eb13a921800f0c07
Robert James Hernandez <rob@sarcasticadmin.com>
2022-04-22 22:41:04 -0700

    move libax25 to static linked lib

--- pkgs/libax25/default.nix
+++ pkgs/libax25/default.nix
@@ -1,8 +1,10 @@
-{ lib, stdenv }:
+{ lib, stdenv, glibc }:
 
 stdenv.mkDerivation rec {
   pname = "libax25";
   version = "0.0.12-rc5";
+
+  buildInputs = [ glibc glibc.static ];
 
   # Sources for ax25 can be found here: https://www.linux-ax25.org/pub/
   src = fetchTarball {
@@ -10,6 +12,8 @@ stdenv.mkDerivation rec {
     sha256 = "1xnxab6n8w8b00dmg5idb6xwqs3llqsl0wv6s3xkf72pn1p1f7k5";
   };
 
+  # Build libax25 staticly since downstream pat wants it to be static
+  LDFLAGS="-static-libgcc -static";
   meta = with lib; {
     description = "A set of functions making it easier to write hamradio programs";
     homepage = "https://www.linux-ax25.org/wiki/Libax25";
