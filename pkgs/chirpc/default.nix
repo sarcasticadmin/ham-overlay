639a64a8ec779ded995e949bfb04cfdab0f9b5a1
Robert James Hernandez <rob@sarcasticadmin.com>
2022-08-09 07:28:18 -0700

    WIP on python2 chirpc

--- pkgs/chirpc/default.nix
+++ pkgs/chirpc/default.nix
@@ -1,23 +1,31 @@
 { lib
-, fetchhg
+, fetchFromGitHub
 , python2
 }:
 
-let
-  src = ./chirp.hg;
-
-in
 python2.pkgs.buildPythonApplication rec {
-  inherit src;
-  pname = "chirp-daily";
-  version = "20220727";
+  pname = "chirprc";
+  version = "20220807";
 
-  #src = fetchurl {
+  src = fetchFromGitHub {
+    owner = "sarcasticadmin";
+    repo = "chirp";
+    #rev = "21924e44d5e7c49bb5bcf0d12e4377194026bba1";
+    #sha256 = "sha256-/g+C6gDPuNpvHmkPChB8cjy5/Z+h4fylFGU3szwF918=";
+    rev = "63f4e8cb975b771f6e29044e5addc1224794bebc";
+    #sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    sha256 = "sha256-HBA0wWLSiKeY+rOXBOJ+wwewQNNIM10BGeeSaJuJ0gQ=";
+  };
   #  rev = "3714:d8618b87d8d5"
   #  url = "http://d-rats.com/hg/chirp.hg";
   #  sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   #};
   #sourceRoot = "${src}/chirpc";
+  pipInstallFlags = ["--install-option=--clionly"];
+  #pipInstallFlags = ["--clionly"];
+  #buildPhase = ''
+  #   ${python2.interpreter} setup.py bdist_wheel --cli
+  #'';
 
   propagatedBuildInputs = with python2.pkgs; [
     pyserial
