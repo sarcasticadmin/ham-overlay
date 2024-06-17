cf891e89002ce70eed2afaf65cae64637059c8ac
Robert James Hernandez <rob@sarcasticadmin.com>
2024-06-17 23:44:47 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -46,8 +46,8 @@
         mheardd = import ./modules/mheardd.nix;
       };
       checks.x86_64-linux = {
-        ax25 = pkgs.testers.runNixOSTest ./tests/ax25.nix
-      };'
+        ax25 = pkgs.testers.runNixOSTest ./tests/ax25.nix;
+      };
     };
 
   # Bold green prompt for `nix develop`
--- tests/ax25.nix
+++ tests/ax25.nix
@@ -1,18 +1,73 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
-in
-  {
-      name = "core";
-      nodes = {
-              # temporary router since we do not have the junipers for ipv6 router advertisement
-              router = { ... }: {
-                      systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
-              };
+  createAX25Node = nodeId: {
+      imports = [
+        ../modules/ax25d.nix
+        ../modules/axlistend.nix
+        ../modules/mheardd.nix
+      ];
+      boot.kernelPatches = [{
+        name = "ax25-ham";
+        patch = null;
+        extraStructuredConfig = with lib.kernel; {
+          HAMRADIO = lib.kernel.yes;
+          AX25 = lib.kernel.module;
+        };
+      }];
+      # TODO
+      #DISABLE FIREWALL
+      # Needed to ensure that module is loaded
+      boot.kernelModules = [ "ax25" ];
+      environment.systemPackages = with pkgs; [
+        libax25
+        ax25-tools
+        ax25-apps
+        socat
+        tncattach
+      ];
+      systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
+      environment.etc."ax25/axports" = {
+        text = ''
+          # me callsign speed paclen window description
+          #
+          wl2k nocall-${toString nodeId} 57600 255 7 Winlink
+        '';
+
+        # The UNIX file mode bits
+        mode = "0644";
       };
-      testScript = { nodes, ... }:
+      services.ax25d = {
+        enable = true;
+        #kissScript = pkgs.writeScript "kissScript" ''
+        #  #!${pkgs.runtimeShell}
+        #  ${pkgs.ax25Tools}/bin/kissattach /dev/ham0 wl2k
+        #  ${pkgs.ax25Tools}/bin/kissparms -p wl2k -t 300 -l 10 -s 12 -r 80 -f n
+        #'';
+      };
+      services.axlistend = {
+    enable = true;
+  };
+     services.mheardd = {
+    enable = true;
+  };
+    };
+in
+{
+  name = "ax25simple";
+  nodes = {
+    node1 = createAX25Node 1;
+    node2 = createAX25Node 2;
+  };
+  testScript = { nodes, ... }:
     ''
       start_all()
-      router.wait_for_unit("systemd-networkd-wait-online.service")
+      node1.succeed("lsmod | grep ax25")
+      node1.succeed("socat -d -d pty,link=/dev/ninotnc,b57600,rawer,echo=0 tcp4-listen:1234 &")
+      node1.wait_for_unit("pgrep socat")
+      node1.succeed("systemctl restart ax25d.service")
+      node2.succeed("socat -d -d pty,link=/dev/ninotnc,b57600,rawer,echo=0 tcp:192.168.1.1:1234 &")
+      node2.wait_for_unit("pgrep socat")
+      node2.succeed("systemctl restart ax25d.service")
     '';
 
 }
